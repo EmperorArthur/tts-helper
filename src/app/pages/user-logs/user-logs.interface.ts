18d15578f229149ec75409dc94be9ebf96d822eb
Arthur Moore <git@cd-net.net>
2026-05-07 18:41:17 -0400

    LogService now implements the Logger interface

--- src/app/pages/user-logs/user-logs.interface.ts
+++ src/app/pages/user-logs/user-logs.interface.ts
@@ -1,4 +1,4 @@
-﻿export type LogLevel = 'info' | 'error';
+﻿export type LogLevel = 'trace' | 'debug' | 'info' | 'warn' | 'error';
 
 export interface IUserLog {
   origin: string,
--- src/app/shared/services/logs.service.ts
+++ src/app/shared/services/logs.service.ts
@@ -1,9 +1,10 @@
 ﻿import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { Logger } from "@smithy/types";
 import { IUserLog, LogLevel } from '../../pages/user-logs/user-logs.interface';
 
 @Injectable()
-export class LogService {
+export class LogService implements Logger {
   readonly logs$ = new BehaviorSubject<IUserLog[]>([]);
 
   add(message: string, level: LogLevel, origin: string) {
@@ -11,6 +12,26 @@ export class LogService {
       ...this.logs$.value,
       { message, level, origin, createdAt: new Date() },
     ]);
+  }
+
+  trace(message: string, origin?: string) {
+    this.add(message, 'trace', origin ?? '');
+  }
+
+  debug(message: string, origin?: string) {
+    this.add(message, 'debug', origin ?? '');
+  }
+
+  info(message: string, origin?: string) {
+    this.add(message, 'info', origin ?? '');
+  }
+
+  warn(message: string, origin?: string) {
+    this.add(message, 'warn', origin ?? '');
+  }
+
+  error(message: string, origin?: string) {
+    this.add(message, 'error', origin ?? '');
   }
 
   /**
